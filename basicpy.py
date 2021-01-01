e991b7eafc6bbff119c56fc03cdcccd96fb5ab43
devopsuser6251 <62200043+devopsuser6251@users.noreply.github.com>
2021-01-01 12:25:20 +0530

    Create basicpy.py

---
+++ basicpy.py
@@ -0,0 +1,44 @@
+print('Python is easy')
+#This is a small python script
+'''
+Multi line comment
+'''
+a=10
+k=20.23
+s="Done"
+a=13
+b=100
+c=-66
+print(a,b,c)
+print(type(k))
+d=3+5j
+print(type(d))
+e=0B1010
+print(e)
+print(type(e))
+f=0XFF
+print(f,type(f))
+g=True
+print(type(g))
+print(9>10)
+print(bool(0))
+print(bool(1))
+print(int(k))
+i=float("22.5")
+print(i)
+print(type(i))
+print(bin(10))
+print(hex(10))
+print(oct(10))
+s='You are awesome'
+print(s)
+s1="""
+You are the creator of your own destiny
+"""
+print(s1)
+print(s[2])
+print(s*4)
+print(len(s1))
+print(len(s))
+
+
